a62a33f4aa8b41da890faf59b526f35d0407051b
Lê Trần Thái Tâm <letranthaitam@gmail.com>
2024-08-23 14:43:11 +0700

    add handlebars

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import express from "express";
+import morgan from "morgan";
+import path from "path";
+import { engine } from "express-handlebars";
 
 const app = express();
-
 const port = 3030;
 
+app.use(morgan("combined"));
+app.engine(
+  "hds",
+  engine({
+    extname: ".hds",
+    layoutsDir: path.join(__dirname, "./resources/views/layouts"),
+    partialsDir: path.join(__dirname, "./resources/views/partials"),
+  })
+);
+app.set("view engine", "hds");
+app.set("views", path.join(__dirname, "./resources/views"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home", { layout: "main" });
+});
+
+app.get("/tin-tuc", (req, res) => {
+  res.send("Tin tức");
 });
 
 app.listen(port, () => {
